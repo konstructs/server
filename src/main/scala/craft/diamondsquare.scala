bffbe37540253d68b28a4435053fac045ef10233
Petter Arvidsson <petter.arvidsson@gmail.com>
2015-04-05 13:38:38 +0200

    Add world generation based on heightmaps
    - Add flat heightmap
    - Add image based heightmap
    - Add adaptive diamond-square heightmap that adapts to existing
      heightmaps
    - Use heightmap to generate terrain
    - Use different terrain types based on height

---
+++ src/main/scala/craft/diamondsquare.scala
@@ -0,0 +1,163 @@
+package craft
+
+import scala.util.Try
+import com.sksamuel.scrimage.Image
+
+trait HeightMap extends PartialFunction[Position, Int]
+
+trait LocalHeightMap {
+  def get(local: LocalPosition): Int
+  def sizeX: Int
+  def sizeZ: Int
+}
+
+case class FlatHeightMap(height: Int) extends HeightMap {
+  def apply(pos: Position) = height
+  def isDefinedAt(pos: Position) = true
+}
+
+case object EmptyHeightMap extends HeightMap {
+  def apply(pos: Position) = ???
+  def isDefinedAt(pos: Position) = false
+}
+
+case class ImageHeightMap(img: Image, range: Int = 128) extends LocalHeightMap {
+  private val scale: Double = (256*256*256) / range
+  def get(local: LocalPosition) = {
+    val v: Double = (img.pixel(local.x, local.z) & 0x00FFFFFF)
+    (v / scale).toInt
+  }
+  def sizeX = img.width
+  def sizeZ = img.height
+}
+
+case class GlobalHeightMap(placement: Position, map: LocalHeightMap) extends HeightMap {
+  def apply(position: Position) = {
+    val x = position.x - placement.x
+    val z = position.z - placement.z
+    map.get(LocalPosition(x, 0, z))
+  }
+  def isDefinedAt(position: Position) =
+    (position.x >= placement.x &&
+      position.z >= placement.z &&
+      position.x < placement.x + map.sizeX &&
+      position.z < placement.z + map.sizeZ)
+}
+
+class DiamondSquareHeightMap(roughness: Float, baseSize: Int, placement: Position, global: PartialFunction[Position, Int]) extends HeightMap {
+  import DiamondSquareHeightMap._
+  private val size = findNearestPowerOfTwo(baseSize) + 1
+  private val offset = size / 4 + 1
+  private val localPlacement = placement.copy(x = placement.x - offset,
+                                              z = placement.z - offset)
+  private val max = size - 1
+  private val array = Array.fill[Float](size * size)(Float.NaN)
+  private val random = new scala.util.Random()
+
+  private val local = new PartialFunction[LocalPosition, Float] {
+    def apply(local: LocalPosition) = {
+      array(local.x + size * local.z)
+    }
+    def isDefinedAt(local: LocalPosition) =
+      array.isDefinedAt(local.x + size * local.z)
+  }
+
+  val g = new PartialFunction[LocalPosition, Float] {
+    def apply(local: LocalPosition) = {
+      val noise = (1.0f - (random.nextFloat() * 2.0f))
+      global(Position(localPlacement.x + local.x, 0, localPlacement.z + local.z)).toFloat + noise
+    }
+    def isDefinedAt(local: LocalPosition) =
+      global.isDefinedAt(Position(localPlacement.x + local.x, 0, localPlacement.z + local.z))
+  }
+
+  val result = GlobalHeightMap(placement, new LocalHeightMap {
+
+    def get(local: LocalPosition): Int =
+      math.round(array((local.x + offset) + size * (local.z + offset)))
+
+    def sizeX = baseSize
+    def sizeZ = baseSize
+  })
+
+  private val map = g orElse local
+
+  {
+    val dist = offset / 2
+    setPoint(0, 0, diamond(0, 0, dist))
+    setPoint(0, max, diamond(0, max, dist))
+    setPoint(max, 0, diamond(max, 0, dist))
+    setPoint(max, max, diamond(max, max, dist))
+    divide(max)
+  }
+
+  private def getPoint(x: Int, z: Int): Option[Float] = {
+    val pos = LocalPosition(x, 0, z)
+    if(map.isDefinedAt(pos)) {
+      val point = map(pos)
+      if(point.isNaN) None
+      else Some(point)
+    }
+    else None
+  }
+
+  private def setPoint(x: Int, z: Int, value: Float) {
+    array(x + size * z) = value
+  }
+
+  def square(x: Int, y: Int, size: Int) =
+    average(
+      getPoint(x - size, y - size), // upper left
+      getPoint(x + size, y - size), // upper right
+      getPoint(x + size, y + size), // lower right
+      getPoint(x - size, y + size) // lower left
+    )
+
+  def diamond(x: Int, y: Int, size: Int) =
+    average(
+      getPoint(x, y - size), // top
+      getPoint(x + size, y), // right
+      getPoint(x, y + size), // bottom
+      getPoint(x - size, y) // left
+    )
+
+  def average(values: Option[Float]*): Float = {
+    val v = values.flatten
+    if(v.size > 0) v.sum / v.size
+    else 0.0f
+  }
+
+  def divide(size: Int) {
+    val half = size / 2
+    val scale = roughness * half
+
+    if (half < 1) return
+
+    for (y <- half until max by size; x <- half until max by size) {
+      val v = square(x, y, half) + random.nextFloat() * scale * 2 - scale
+      setPoint(x, y, v)
+    }
+    for (y <- 0 to max by half; x <- ((y + half) % size) to max by size) {
+      val v = diamond(x, y, half) + random.nextFloat() * scale * 2 - scale
+      setPoint(x, y, v)
+    }
+    divide(size / 2)
+  }
+
+  def apply(pos: Position): Int = result(pos)
+  def isDefinedAt(pos: Position) = result.isDefinedAt(pos)
+
+}
+
+object DiamondSquareHeightMap {
+  def findNearestPowerOfTwo(number: Int): Int = {
+    if(number < 0) throw new IllegalArgumentException("Number must be positive")
+    var n = number
+    var i = 0
+    while(n != 0) {
+      i += 1
+      n = n >> 1
+    }
+    1 << i
+  }
+}
--- src/main/scala/craft/generator.scala
+++ src/main/scala/craft/generator.scala
@@ -1,7 +1,9 @@
 package craft
 
+import java.io.File
 import scala.util.Random
 import akka.actor.{ Actor, Props }
+import com.sksamuel.scrimage.Image
 
 case class Bounds(pMin: Int, qMin: Int, kMin: Int, pMax: Int, qMax: Int, kMax: Int) {
   def contains(chunk: ChunkPosition) =
@@ -14,46 +16,63 @@ class GeneratorActor extends Actor {
 
   val rnd = new Random
 
-  val bounds = Bounds(-10, -10, -10, 10, 10, 10)
-
-  val blocks = Set(2,4,5,6,8,11,12,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,52,53,54,55,56,57,58,59,60,61,62,63).toVector
-
   val compressionBuffer = new Array[Byte](ChunkSize * ChunkSize * ChunkSize)
 
-  private def blockSeq(chunk: ChunkPosition): Seq[Byte] =
+  val size = 1024
+
+  val diamond0 = new DiamondSquareHeightMap(0.1f, size, Position(-size, 0, -size), EmptyHeightMap)
+
+  val diamond1 = new DiamondSquareHeightMap(0.2f, size, Position(-size, 0, 0), diamond0)
+
+  val diamond2 = new DiamondSquareHeightMap(0.3f, size, Position(-size, 0, size), diamond0 orElse diamond1)
+
+  val diamond3 = new DiamondSquareHeightMap(0.4f, size, Position(0, 0, -size), diamond0 orElse diamond1 orElse diamond2)
+
+  val diamond4 = new DiamondSquareHeightMap(0.5f, size, Position(0, 0, 0), diamond0 orElse diamond1 orElse diamond2 orElse diamond3)
+
+  val diamond5 = new DiamondSquareHeightMap(0.6f, size, Position(0, 0, size), diamond0 orElse diamond1 orElse diamond2 orElse diamond3 orElse diamond4)
+
+  val diamond6 = new DiamondSquareHeightMap(0.7f, size, Position(size, 0, -size), diamond0 orElse diamond1 orElse diamond2 orElse diamond3 orElse diamond4 orElse diamond5)
+
+  val diamond7 = new DiamondSquareHeightMap(0.8f, size, Position(size, 0, 0), diamond0 orElse diamond1 orElse diamond2 orElse diamond3 orElse diamond4 orElse diamond5 orElse diamond6)
+
+  val diamond8 = new DiamondSquareHeightMap(0.9f, size, Position(size, 0, size), diamond0 orElse diamond1 orElse diamond2 orElse diamond3 orElse diamond4 orElse diamond5 orElse diamond6 orElse diamond7)
+
+
+  val map = diamond0 orElse diamond1 orElse diamond2 orElse diamond3 orElse diamond4 orElse diamond5 orElse diamond6 orElse diamond7 orElse diamond8 orElse FlatHeightMap(12)
+  val random = new scala.util.Random
+  private def blockSeq(chunk: ChunkPosition): Seq[Byte] = {
     for(
-      x <- 0 until ChunkSize;
+      z <- 0 until ChunkSize;
       y <- 0 until ChunkSize;
-      z <- 0 until ChunkSize
+      x <- 0 until ChunkSize
     ) yield {
-      if (chunk.k < 1) {
-        44.toByte
-      } else if(chunk.p == bounds.pMin || chunk.p == bounds.pMax || chunk.q == bounds.qMin || chunk.q == bounds.qMax) {
-        if(chunk.k < 3)
-          58.toByte
-        else
-          0.toByte
-      } else if(chunk.k < 2 || (chunk.k < 3 && y < ChunkSize / 2)){
-        if(rnd.nextInt(10) < 8)
+      val pos = LocalPosition(x, y, z)
+      val global = pos.global(chunk)
+      val height = map(global) + 32
+      if(global.y < height) {
+        if(global.y == height - 1) {
+          if(global.y + random.nextInt(10) - 5 < 64)
+            1.toByte
+          else
+            9.toByte
+        } else if(global.y > height - 10) {
+          7.toByte
+        } else {
           6.toByte
-        else
-          blocks(rnd.nextInt(blocks.size)).toByte
-      } else if(chunk.k < 3) {
-        7.toByte
-      } else if(chunk.k < 4 && y == 0) {
-        1.toByte
+        }
       } else {
         0.toByte
       }
     }
+  }
 
   private def blocks(chunk: ChunkPosition): Array[Byte] =
     compress.deflate(Array( blockSeq(chunk) :_*), compressionBuffer)
 
   def receive = {
     case Generate(chunk) =>
-      if(bounds contains chunk)
-        sender ! Generated(chunk, blocks(chunk))
+      sender ! Generated(chunk, blocks(chunk))
   }
 }
 
