cb1155d6ab75ec16756dec8a0c1952a2699d7d57
Petter Arvidsson <petter.arvidsson@gmail.com>
2015-06-28 13:59:59 +0200

    Add drop method
    - Also add comments describing the functionality of filter

--- src/main/scala/konstructs/filter.scala
+++ src/main/scala/konstructs/filter.scala
@@ -7,19 +7,35 @@ trait Filter[T] {
   def next(chain: Seq[ActorRef]): Filter[T]
   def next(chain: Seq[ActorRef], message: T): Filter[T]
 
+  /** Let next plugin in chain handle the unchanged message
+    */
   def continue(implicit sender: ActorRef) {
     chain.head ! next(chain = chain.tail)
   }
 
+  /** Let next plugin in chain handle an updated message
+    */
   def continueWith(newMessage: T)(implicit sender: ActorRef) {
     chain.head ! next(chain = chain.tail, message = newMessage)
   }
 
+  /** Skip all other plugins in chain, but let the server process the
+    * unchanged message
+    */
   def skip(implicit sender: ActorRef) {
     chain.last ! next(chain = Seq())
   }
 
+  /** Skip all other plugins in chain, but let the server process an
+    * updated message
+    */
   def skipWith(newMessage: T)(implicit sender: ActorRef) {
     chain.last ! next(chain = Seq(), message = newMessage)
   }
+
+  /** Drop the message
+    * (this is a no-op since the message is dropped if continue or
+    * skip is not called)
+    */
+  def drop() {}
 }
