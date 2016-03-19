7176b5897dac17f905d29d5ae7ae3b00ecc959fc
Petter Arvidsson <petter.arvidsson@gmail.com>
2016-03-19 13:46:11 +0100

    Add package-info docs

---
+++ src/main/java/konstructs/api/messages/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Messages are classes that represents messages that can be sent to
+ * or received from the Konstructs API. All messages are immutable,
+ * Please see the link below to an excellent introduction to
+ * immutability by Oracle themselves.
+ *
+ * @see <a href="https://docs.oracle.com/javase/tutorial/essential/concurrency/immutable.html">Oracles's guide to immutability</a>
+ */
+
+package konstructs.api.messages;
